a1e0e85a0d394a0f844ee54039c34f3750714035
beraldoLucca <lucca.beraldo@aluno.ifsp.edu.br>
2025-03-19 16:57:04 -0300

    update version

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,8 +6,6 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-
-  depends_on = [aws_eks_cluster.fiap]
 }
 
 data "aws_eks_cluster" "cluster" {
@@ -16,4 +14,12 @@ data "aws_eks_cluster" "cluster" {
 
 data "aws_eks_cluster_auth" "cluster" {
   name = aws_eks_cluster.fiap.name
+}
+
+resource "null_resource" "configure_kubeconfig" {
+  depends_on = [aws_eks_cluster.fiap]
+
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --name ${aws_eks_cluster.fiap.name} --region us-east-1"
+  }
 }

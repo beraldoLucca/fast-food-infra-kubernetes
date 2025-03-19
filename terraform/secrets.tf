356f979314d66781b240131f6e739bb98727b1dd
beraldoLucca <lucca.beraldo@aluno.ifsp.edu.br>
2025-03-19 08:41:27 -0300

    added service secret

---
+++ terraform/secrets.tf
@@ -0,0 +1,11 @@
+resource "aws_secretsmanager_secret" "eks_service_url" {
+  name        = "eks-service-url"
+  description = "Secret containing the URL of the EKS service load balancer"
+}
+
+resource "aws_secretsmanager_secret_version" "eks_service_url_version" {
+  secret_id     = aws_secretsmanager_secret.eks_service_url.id
+  secret_string = jsonencode({
+    service_url = "https://${data.kubernetes_service.fast_food_service.status[0].load_balancer[0].ingress[0].hostname}"
+  })
+}

c943abe516947c4a711a1aa7f0ed9c78a59f2a76
beraldoLucca <lucca.beraldo@aluno.ifsp.edu.br>
2025-03-20 18:21:22 -0300

    update

--- terraform/eks-access-policy.tf
+++ terraform/eks-access-policy.tf
@@ -8,26 +8,17 @@ resource "aws_eks_access_policy_association" "policy" {
   }
 }
 
+data "aws_iam_policy" "secrets_manager_policy" {
+  name = "EKSSecretsManagerPolicy"
+  arn    = var.policyArn
+}
+
 resource "aws_eks_access_policy_association" "secrets_policy" {
   cluster_name  = aws_eks_cluster.fiap.name
-  policy_arn    = aws_iam_policy.secrets_manager_policy.arn  # Aqui usamos a policy criada acima
+  policy_arn    = data.aws_iam_policy.secrets_manager_policy.arn  # Referencia a policy existente
   principal_arn = var.principalArn  # Essa variável deve apontar para a ARN da role do EKS
 
   access_scope {
     type = "cluster"
   }
-}
-
-resource "aws_iam_policy" "secrets_manager_policy" {
-  name        = "EKSSecretsManagerPolicy"
-  description = "Policy to allow EKS to access AWS Secrets Manager"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect   = "Allow"
-      Action   = ["secretsmanager:GetSecretValue"]
-      Resource = aws_secretsmanager_secret.aws_credentials.arn
-    }]
-  })
 }

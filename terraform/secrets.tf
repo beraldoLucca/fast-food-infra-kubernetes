a54d66e8e9c96e780afc8c2d915d72fd75df8a57
beraldoLucca <lucca.beraldo@aluno.ifsp.edu.br>
2025-03-20 18:39:46 -0300

    update

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -14,7 +14,7 @@ resource "aws_secretsmanager_secret_version" "eks_service_url_version" {
 }
 
 resource "aws_secretsmanager_secret" "aws_credentials" {
-  name        = "aws-secret-4"
+  name        = "aws-secret-5"
   description = "AWS credentials for EKS pods"
 }
 

244e64dda8b1e31349e809dfab742caad7b5313d
beraldoLucca <lucca.beraldo@aluno.ifsp.edu.br>
2025-03-20 13:16:54 -0300

    update deploy file

--- terraform/eks-access-policy.tf
+++ terraform/eks-access-policy.tf
@@ -7,3 +7,37 @@ resource "aws_eks_access_policy_association" "policy" {
     type = "cluster"
   }
 }
+
+resource "aws_iam_role" "secrets_manager_role" {
+  name = "eks-secrets-manager-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "secrets_manager_policy" {
+  name        = "EKSSecretsManagerPolicy"
+  description = "Policy to allow EKS to access AWS Secrets Manager"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["secretsmanager:GetSecretValue"]
+      Resource = aws_secretsmanager_secret.aws_credentials.arn
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_secrets_manager_policy" {
+  role       = aws_iam_role.secrets_manager_role.name
+  policy_arn = aws_iam_policy.secrets_manager_policy.arn
+}
